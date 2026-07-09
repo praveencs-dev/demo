a26d4e3d3a72ce6c7883a56d4e9949fcdbabad72
praveencs-dev <praveensculpture@gmail.com>
2026-07-09 13:12:21 +0530

    files added

--- Model/loginModel.js
+++ Model/loginModel.js
@@ -1,7 +1,7 @@
 const db = require('../config/db');
 
 async function login(name, password) {
-    return (await db.query("SELECT id,name FROM users WHERE id=$1 AND password=$2",[name,password])).rows
+    return (await db.query("SELECT id,name,role FROM users WHERE name=$1 AND password=$2",[name,password])).rows
 }
 async function stafflogin(id, password) {
     return (await db.query("SELECT id,name,role FROM staffs WHERE id=$1 AND password=$2",[id,password])).rows
